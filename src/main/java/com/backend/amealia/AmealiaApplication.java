2b9a7162e804e699f965f34693de4c99b20033ed
Adedotun-Adepoju <d.e.adepoju@gmail.com>
2026-01-17 22:48:37 +0100

    initial setup

---
+++ src/main/java/com/backend/amealia/AmealiaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.amealia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmealiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmealiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/amealia/AmealiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.amealia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmealiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
